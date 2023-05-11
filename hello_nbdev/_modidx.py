372c5f72957a61040f47b164c320d35345394c30
Johan Euphrosine <proppy@google.com>
2023-05-11 14:24:57 +0200

    Add `say_hello`; update index

--- hello_nbdev/_modidx.py
+++ hello_nbdev/_modidx.py
@@ -5,4 +5,5 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://proppy.github.io',
                 'git_url': 'https://github.com/proppy/hello-nbdev',
                 'lib_path': 'hello_nbdev'},
-  'syms': {'hello_nbdev.core': {'hello_nbdev.core.foo': ('core.html#foo', 'hello_nbdev/core.py')}}}
+  'syms': { 'hello_nbdev.core': { 'hello_nbdev.core.foo': ('core.html#foo', 'hello_nbdev/core.py'),
+                                  'hello_nbdev.core.say_hello': ('core.html#say_hello', 'hello_nbdev/core.py')}}}
